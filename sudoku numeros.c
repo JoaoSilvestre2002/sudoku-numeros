5e34cf4bc0be4dcf363df2ee7a01ced79150aeae
JoaoSilvestre2002 <92168242+JoaoSilvestre2002@users.noreply.github.com>
2022-01-13 18:26:33 -0300

    Update sudoku numeros.c

--- sudoku numeros.c
+++ sudoku numeros.c
@@ -2,67 +2,67 @@
 //Joao Silvestre
 #define dimensao 9
 
-void zeraDigitos(int *dg) {
+void digitnule(int *gd) {
     int controle1;
     for (controle1 = 0; controle1 < dimensao; controle1++)
-        dg[controle1] = 0;
+        gd[controle1] = 0;
 }
 
-int valida(int v[dimensao][dimensao], int flag, int *dg) {
-    int controle1, j;
+int funcao(int v[dimensao][dimensao], int x, int *gd) {
+    int controle2, controle1;
     for (controle1 = 0; controle1 < dimensao; controle1++) {
-        for (j = 0; j < dimensao; j++) {
-            if (flag) {
-                dg[v[controle1][j]]++;
-                if (dg[v[controle1][j]] > 1)
+        for (controle2 = 0; controle2 < dimensao; controle2++) {
+            if (x) {
+                gd[v[controle1][controle2]]++;
+                if (gd[v[controle1][controle2]] > 1)
                     return 1;
             }
             else {
-                dg[v[j][controle1]]++;
-                if (dg[v[j][controle1]] > 1)
+                gd[v[controle2][controle1]]++;
+                if (gd[v[controle2][controle1]] > 1)
                     return 1;
             }
         }
-	zeraDigitos(dg);
+	digitnule(gd);
     }
     return 0;
 }
 
-int validaSubMatrix(int v[dimensao][dimensao], int lInicio, int lFim, int cInicio, int cFim, int *dg) {
-    int controle1, j;
+int confirm2matrix(int v[dimensao][dimensao], int lInicio, int lFim, int cInicio, int cFim, int *gd) {
+    int controle1, controle2;
     for (controle1 = lInicio; controle1 < lFim; controle1++) {
-        for (j = cInicio; j < cFim; j++) {
-	    dg[v[j][controle1]]++;
-	    if (dg[v[j][controle1]] > 1)
+        for (controle2 = cInicio; controle2 < cFim; controle2++) {
+	    gd[v[controle2][controle1]]++;
+	    if (gd[v[controle2][controle1]] > 1)
 		return 1;
 	}
     }
-    zeraDigitos(dg);
+    digitnule(gd);
     return 0;
 }
 
 int main() {
 
-    int n, mat[dimensao][dimensao], invalid, digits[dimensao], cases, controle1, j;
-    scanf("%i", &n);
+    int number, principalmatriz[dimensao][dimensao], negacao, digits[dimensao], circunstancia, controle2, controle1;
+    scanf("%i", &number);
 
-    for (cases = 1; cases <= n; cases++) {
+    for (circunstancia = 1; circunstancia <= number; circunstancia++) {
         for (controle1 = 0; controle1 < dimensao; controle1++) {
-	    zeraDigitos(digits);
-	    for (j = 0; j < dimensao; j++) {
-		scanf("%i", &mat[controle1][j]);
-		mat[controle1][j]--;
+	    digitnule(digits);
+	    for (controle2 = 0; controle2 < dimensao; controle2++) {
+		scanf("%i", &principalmatriz[controle1][controle2]);
+		principalmatriz[controle1][controle2]--;
 	    }
 	}
-	invalid = valida(mat, 1, digits) + valida(mat, 0, digits);
+	negacao = funcao(principalmatriz, 1, digits) + funcao(principalmatriz, 0, digits);
 
 	for (controle1 = 0; controle1 < dimensao; controle1+=3)
-            for (j = 0; j < dimensao; j+=3)
-    		invalid += validaSubMatrix(mat, controle1, controle1 + 3, j, j + 3, digits);
+            for (controle2 = 0; controle2 < dimensao; controle2+=3)
+    		negacao += confirm2matrix(principalmatriz, controle1, controle1 + 3, controle2, controle2 + 3, digits);
 
-	printf("Instancia %i\n", cases);
+	printf("Instancia %i\n", circunstancia);
 
-	if (invalid)
+	if (negacao)
 	    printf("NAO\n\n");
 	else
 	    printf("SIM\n\n");
